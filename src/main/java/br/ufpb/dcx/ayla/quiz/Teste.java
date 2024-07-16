002cf3a1409b30de5e78a633e6550b6ecf20f439
jose.santos <jose.santos@dcx.ufpb.br>
2024-07-16 09:47:09 -0300

    Versão Funcional SistemaQuiz

--- src/main/java/br/ufpb/dcx/ayla/quiz/Teste.java
+++ src/main/java/br/ufpb/dcx/ayla/quiz/Teste.java
@@ -4,4 +4,6 @@ public class Teste {
     public static void main(String[] args) {
         System.out.println("Hello world");
         Pergunta pergunta = new PerguntaDissertativa();
+        System.out.println(pergunta.toString());
+
 }}
