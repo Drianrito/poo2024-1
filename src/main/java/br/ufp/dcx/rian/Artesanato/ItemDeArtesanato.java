eef0a46c9d63b0a076b3cf8f6dc9316cfb2550b1
jose.santos <jose.santos@dcx.ufpb.br>
2024-07-18 11:21:58 -0300

    Adicao do programa artesanato

--- src/main/java/br/ufp/dcx/rian/Artesanato/ItemDeArtesanato.java
+++ src/main/java/br/ufp/dcx/rian/Artesanato/ItemDeArtesanato.java
@@ -1,4 +1,41 @@
 package br.ufp.dcx.rian.Artesanato;
 
 public class ItemDeArtesanato {
+    private String codigo;
+    private String nome;
+    private double preco;
+
+    public ItemDeArtesanato(String codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public ItemDeArtesanato() {
+        this("","",0);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
